246be2508346eea38ecff99287d2bd9d6724b7ce
limsungju <fullmoon4860@gmail.com>
2019-08-12 17:33:28 +0900

    java-practice06 - 1번문제 해결

--- src/prob1/Sort.java
+++ src/prob1/Sort.java
@@ -7,6 +7,7 @@ public class Sort {
 		int array[] = { 5, 9, 3, 8, 60, 20, 1 };
 		int temp = 0;
 		int count =  array.length;
+		int count2 = 0;
 		
 		System.out.println( "Before sort." );
 		
@@ -17,16 +18,26 @@ public class Sort {
 		//
 		// 정렬 알고리즘이 적용된 코드를 여기에 작성합니다.
 		//
-
-		
-		
+		while(true) {
+			for(int j=0; j<count-1; j++) {
+				if(array[j+1] > array[j]) {
+					temp = array[j+1];
+					array[j+1] = array[j];
+					array[j] = temp;
+				}
+			}
+			count2++;
+			if(count2==(count-1)) {
+				break;
+			}
+		}
 		
 		
 		System.out.println( "\nAfter Sort." );
 		
 		for (int i = 0; i < count; i++) {
 			System.out.print(array[i] + " ");
-		}		
+		}
 
 	}
 }
