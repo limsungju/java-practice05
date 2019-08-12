ca978448d0faada580107e481c6ad6b5684e125f
limsungju <fullmoon4860@gmail.com>
2019-08-12 17:47:44 +0900

    java-practice05 - 2번문제 수정

--- src/prob2/SmartPhone.java
+++ src/prob2/SmartPhone.java
@@ -5,7 +5,7 @@ public class SmartPhone extends MusicPhone {
 	@Override
 	public void execute(String function) {
 		if(function.equals("음악")) {
-			downLoad();
+			playMusic();
 			return;
 		} else if(function.equals("통화")) {
 			super.execute(function);
@@ -16,10 +16,14 @@ public class SmartPhone extends MusicPhone {
 		}
 	}
 	
-	public void downLoad() {
+	
+	@Override
+	protected void playMusic() {
 		System.out.println("다운로드해서 음악재생");
 	}
-	
+
+
+
 	public void app() {
 		System.out.println("앱실행");
 	}
