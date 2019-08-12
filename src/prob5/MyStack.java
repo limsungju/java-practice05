b431d72ad2327013a53677ca0503e48e41fa9056
limsungju <fullmoon4860@gmail.com>
2019-08-12 21:30:15 +0900

    java-practive05 - 5번문제 해결

--- src/prob5/MyStack.java
+++ src/prob5/MyStack.java
@@ -4,17 +4,40 @@ public class MyStack {
 	private int top = -1;
 	private String[] buffer;
 	
+	public MyStack(int i) {
+		this.top = -1;
+		buffer = new String[i];
+	}
+
 	public String push(String str) {
 		
+		if(top>=(buffer.length-1)) {
+			String[] buffer2 = new String[(buffer.length*2)];
+			for(int i=0; i<buffer.length; i++) {
+				buffer2[i] = buffer[i];
+			}
+			buffer=buffer2;
+		}
+		this.top += 1;
+		buffer[top] = str;
+		return buffer[top];
 		
-		return str;
+		
 	}
 	
-	public void pop() {
+	public String pop() throws MyStackException {
+		if(this.top==-1) {
+			throw new MyStackException("stack is empty");
+		}
+		
+		String val = buffer[top];
+		this.top -= 1;
+		
+		return val;
 		
 	}
 	
-	public void isEmpty() {
-		
+	public boolean isEmpty() {
+		return (this.top < 0) ? true : false;
 	}
 }
